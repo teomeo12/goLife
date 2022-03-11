5109868bc7099238f9afecb95ec7b6f6c8c156b2
Debruyne <lucas0000.milow@gmail.com>
2022-03-11 13:12:37 +0100

    fix login and register page

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,8 +1,15 @@
 import './Home.css';
 import Articles from '../Articles/Articles';
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
 export default function Home() {
     return (
-        <Articles/>
+        <>
+            <Header />
+            <Articles />
+            <Footer />
+        </>
     );
 }
 
