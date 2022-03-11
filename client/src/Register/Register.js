dec7d5bb3775865f45090a224642395d023cd313
Debruyne <lucas0000.milow@gmail.com>
2022-03-11 11:22:09 +0100

    adding register route + register style and content

--- client/src/Register/Register.js
+++ client/src/Register/Register.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Login.css";
 import mail from "../Images/mail.png"
 import password from "../Images/password.png"
 import logo from "../Images/logo3.png"
 
-export default function Login(props) {
+export default function Register(props) {
   const [data, setData] = useState([]);
 
   async function handleSubmit(e) {
@@ -20,7 +19,7 @@ export default function Login(props) {
         <div className="logo_img">
           <img src={logo} />
         </div>
-        <h1>Login</h1>
+        <h1>Sign Up</h1>
       </header>
       <div class="triangle"></div>
       <form onSubmit={props.onSignin}>
@@ -31,46 +30,25 @@ export default function Login(props) {
           </div>
           <div class="container__div">
             <img src={password} />
-            <input type="password" name="password" placeholder="Password" autoComplete="off" ref={props.passwordRef} required />
+            <input type="password" name="password" placeholder="Choose password" autoComplete="off" ref={props.passwordRef} required />
+          </div>
+          <div class="container__div">
+            <img src={password} />
+            <input type="password" name="password" placeholder="Confirm password" autoComplete="off" ref={props.passwordRef} required />
           </div>
           <div>
             <p><a href="" id="Forgot">Forgot password ?</a></p>
           </div>
           <div>
             <button type="submit" name="login" onClick={props.onSignup}>
-              Login
+              Sign Up
             </button>
           </div>
           <div>
-            <p>Don't have an account ? <a href="" id="Sign_upButton">Sign Up</a></p>
+            <p>Already have an account ? <a href="" id="Sign_upButton">Login</a></p>
           </div>
         </div>
       </form>
     </>
   );
 }
-
-
-{/* <Form onSubmit={handleSubmit}>
-  <Form.Group className="mb-3" controlId="title">
-    <Form.Label>User Name</Form.Label>
-    <Form.Control type="text" placeholder="Title" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="content">
-    <Form.Label>Content</Form.Label>
-    <Form.Control type="text" placeholder="Content" />
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="myfile">
-    <Form.Label>Image</Form.Label>
-    <Form.Control type="file" />
-  </Form.Group>
-
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>; */}
